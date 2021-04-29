ab43f8ec734c3317b79239d23865c43fef9f307a
kraevskiy <rambutan@educonlines.com>
2021-04-29 20:22:46 +0300

    add sort get reviews at createdAt

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -54,6 +54,16 @@ export class ProductService {
         $addFields: {
           reviewCount: { $size: '$reviews' },
           reviewAvg: { $avg: '$reviews.rating' },
+          reviews: {
+            $function: {
+              body: `function(review) {
+                review.sort((a, b)=> new Date(b.createdAt) - new Date(a.createdAt));
+                return review;
+              }`,
+              args: ['$reviews'],
+              lang: 'js',
+            },
+          },
         },
       },
     ]).exec() as (ProductModel & { review: ReviewModel[], reviewCount: number, reviewAvg: number })[];
